13a64845b31e4a964ba4ebf96c0d616bcbd2a7a8
Porteries Tristan <republicthunderbolt9@gmail.com>
2016-11-20 17:30:18 +0000

    Include scale in transformation.

--- source/gameengine/Ketsji/KX_BatchGroup.cpp
+++ source/gameengine/Ketsji/KX_BatchGroup.cpp
@@ -65,7 +65,9 @@ void KX_BatchGroup::MergeObjects(const std::vector<KX_GameObject *>& objects)
 			continue;
 		}
 
-		const MT_Transform trans(gameobj->NodeGetWorldPosition(), gameobj->NodeGetWorldOrientation());
+		MT_Transform trans(gameobj->NodeGetWorldPosition(), gameobj->NodeGetWorldOrientation());
+		const MT_Vector3& scale = gameobj->NodeGetWorldScaling();
+		trans.scale(scale.x(), scale.y(), scale.z());
 
 		if (MergeMeshUser(meshUser, trans.toMatrix())) {
 			m_objects->Add(gameobj);
